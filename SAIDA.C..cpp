ffb34c86a50e944e8a69d9fdef30ea175d2b1eaf
Hugo Vieira <108480477+Hugo2022vieira@users.noreply.github.com>
2022-10-07 22:09:17 -0300

    Update SAIDA.C..cpp

--- SAIDA.C..cpp
+++ SAIDA.C..cpp
@@ -5,7 +5,7 @@ int main(void)
 /* sintaxe: tipo_de_dadonome_da_variavel=valor_inicial,*/
 int num=10
 char letra='a';
-printf("CURSO ADV INFORMÁTIVA/n");
+printf("CURSO ADV/n");
 printf("NUMERO:%d-letra:%c\n",num,letra);
 system("PAUSE");
 RETURN 0;
